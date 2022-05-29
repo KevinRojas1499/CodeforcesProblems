7a4a432a0c245d71cb72f46b16834de5a49b57a6
KevinRojas1499 <kevinrojas@pop-os.localdomain>
2022-05-29 08:54:42 -0500

    Added solutions to two problems

--- 1700s/Checkposts.cpp
+++ 1700s/Checkposts.cpp
@@ -1,18 +1,70 @@
 #include <bits/stdc++.h>
-#define ll long long
+#define ll long long 
 
 using namespace std;
 
-vector<ll> order;
+const ll MOD = 1e9+7;
+vector<vector<ll>> adj, rev_adj;
+vector<ll> order, component,seen;
 
-ll dfs1(){
-
+void dfs1(ll u){
+    if(seen[u]) return;
+    seen[u] = 1;
+    for(auto v: adj[u]) dfs1(v);
+    order.push_back(u);
 }
 
-ll dfs2() {
+void dfs2(ll u){
+    if(seen[u]) return;
+    seen[u] = 1;
+    component.push_back(u);
+    for(auto v : rev_adj[u]) dfs2(v); 
+}
+
+int main () {
+    ll n,m,u,v;
+    cin>>n;
+    adj.resize(n); rev_adj.resize(n); seen.resize(n,false);
+    vector<ll> cost(n);
+    for(int i = 0; i<n; i++) cin>>cost[i];
+    cin>>m;
+    for(int i = 0; i<m; i++){
+        cin>>u>>v;
+        u--;v--;
+        adj[u].push_back(v);
+        rev_adj[v].push_back(u);
+    }
+
+    for(int i = 0; i<n; i++){
+        if(!seen[i]) dfs1(i);
+    }
+    reverse(order.begin(),order.end());
+    seen.assign(n,false);
+    ll res = 0, numberOfWays = 1;
+    for(int i = 0; i<n; i++){
+        u = order[i];
+        if(!seen[u]){
+            dfs2(u);
+        }
+        else {
+            continue;
+        }
+        ll minimo = cost[u];
+        for(auto a : component){
+            minimo = min(minimo, cost[a]);
+        }
+        ll numMin = 0;
+        for(auto a : component){
+            if(cost[a] == minimo) numMin++;
+        }
+        res+=minimo;
+        numberOfWays*=numMin;
+        numberOfWays%=MOD;
+        component.clear();
+    }
+    numberOfWays+=MOD;
+    numberOfWays%=MOD;
+    cout<<res<<" "<<numberOfWays<<"\n";
+
     
-}
-
-int main() {
-
 }
---
+++ 1700s/SleepingSchedule.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll long long 
+
+using namespace std;
+
+ll n,h,l,r, dp[2005][2005], a[2005], seen[2005][2005];
+
+ll mod(ll u){
+    return (u%h+h)%h;
+}
+
+ll solve(ll u, ll t){
+    if(seen[u][t]) return dp[u][t];
+    if(u == n) return 0;
+    seen[u][t] = 1;
+    ll bef = mod(t+a[u]-1+h), aft = mod(t+a[u]+h);
+    ll res = max(solve(u+1,aft),solve(u+1,bef));
+    if(l<= mod(t+a[u]-1) && mod(t+a[u]-1)<=r) res = max(res,1+solve(u+1,bef));
+    if(l<= mod(t+a[u]) && mod(t+a[u]) <=r) res = max(res,1+solve(u+1,aft));
+    return dp[u][t] = res;
+}
+
+int main() {
+    cin>>n>>h>>l>>r;
+    memset(dp,0,sizeof dp);
+    memset(seen,0,sizeof seen);
+    for(int i = 0; i<n; i++) {
+        cin>>a[i];
+    }
+
+    cout<<solve(0,0)<<"\n";
+
+    
+
+    
+}
